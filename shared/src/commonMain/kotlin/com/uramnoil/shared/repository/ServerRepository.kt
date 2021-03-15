43b93f888c43915c0ad357444802fff2e9a2b0c1
uramnoil <uramnoil@outlook.jp>
2021-03-15 22:30:52 +0900

    削除

--- shared/src/commonMain/kotlin/com/uramnoil/shared/repository/ServerRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.uramnoil.shared.repository
-
-import com.uramnoil.shared.Server
-import com.uramnoil.shared.ServerListLoader
-
-interface ServerRepository {
-	suspend fun getServers(): List<Server>
-}
