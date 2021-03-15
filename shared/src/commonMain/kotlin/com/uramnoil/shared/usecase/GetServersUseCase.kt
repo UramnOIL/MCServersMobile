21d4408e1efa196c047bf60555aef7c46c5d0708
uramnoil <uramnoil@outlook.jp>
2021-03-15 22:30:43 +0900

    アクセサの変更

--- shared/src/commonMain/kotlin/com/uramnoil/shared/usecase/GetServersUseCase.kt
+++ shared/src/commonMain/kotlin/com/uramnoil/shared/usecase/GetServersUseCase.kt
@@ -1,18 +1,16 @@
 package com.uramnoil.shared.usecase
 
-import com.uramnoil.shared.repository.ServerRepository
+import com.uramnoil.shared.ServerListService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
-class GetServersUseCase(val output: GetServersOutputPort, val repository: ServerRepository) : CoroutineScope, GetServersInputPort {
+class GetServersUseCase(private val output: GetServersOutputPort, private val repository: ServerListService) : CoroutineScope, GetServersInputPort {
 	override val coroutineContext = Dispatchers.Main
 
 	override fun getServers() {
 		launch {
-			output.setServers(repository.getServers())
+			output.setServers(repository.getServerList())
 		}
 	}
 }
