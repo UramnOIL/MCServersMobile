7aa844ebf73414bf9e68cc108e149a4ff79aa84c
uramnoil <uramnoil@outlook.jp>
2021-03-15 23:13:23 +0900

    リファクタリング

--- shared/src/commonMain/kotlin/com/uramnoil/shared/presentation/presenter/ServerListPresenter.kt
+++ shared/src/commonMain/kotlin/com/uramnoil/shared/presentation/presenter/ServerListPresenter.kt
@@ -1,11 +1,10 @@
 package com.uramnoil.shared.presentation.presenter
 
-import org.koin.core.component.KoinComponent
 
 interface ServerListPresenter {
 	val view: ServerListView
 }
 
-interface ServerListView : KoinComponent {
+interface ServerListView {
 	var presenter: ServerListPresenter?
 }
