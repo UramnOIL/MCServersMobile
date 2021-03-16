9d5ce16fad14f550488d56b06ccb3cb404669b82
uramnoil <uramnoil@outlook.jp>
2021-03-16 17:48:29 +0900

    :heavy_plus_sign: Android用のKodeinの追加

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     id("com.android.application")
+    kotlin("android")
 }
 
 group = "com.uramnoil"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    val kotlinVersion: String by project
+    val kodeinVersion: String by project
 
     implementation(project(":shared"))
-    implementation(kotlin("stdlib", version = kotlinVersion))
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("org.kodein.di:kodein-jvm:${kodeinVersion}")
 }
 
 android {
@@ -29,4 +30,6 @@ android {
             isMinifyEnabled = false
         }
     }
+
+    buildFeatures.viewBinding = true
 }
